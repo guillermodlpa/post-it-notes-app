9f6875e48c580336d3879c94313c99dd0a469390
Guillermo <guillermo@splashthat.com>
2015-08-07 22:43:14 -0400

    make the watch work over vagrant

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -5,6 +5,6 @@ module.exports = function (app) {
 
 	app.get('/', function(request, response){
 		response.writeHead(200, {'Content-Type': 'text/plain'});
-  		response.end('Hello World3!\n');
+		response.end('Hello World1!\n');
 	});
 }
