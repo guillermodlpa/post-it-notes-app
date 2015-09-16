82cd720c9fdfd9eec459d20aa89a5108261d429e
Guillermo <guillermo@splashthat.com>
2015-09-15 23:38:09 -0400

    app.js moved up so public is a sibling directory

--- app/app.js
+++ app/app.js
@@ -12,13 +12,13 @@ app.set( 'view engine', 'ejs');
 app.use( partials() );
 
 // all routes are in controllers/routes.js
-var routes = require('./controllers/routes')(app);
+var routes = require('./server/controllers/routes')(app);
 
 // set public directory for assets like css and js files
 app.use(express.static(__dirname + '/public'));
 
 // explicitly indicate views directory, as it was failing otherwise
-app.set('views', __dirname + "/views");
+app.set('views', __dirname + "/server/views");
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 app.listen(_port, _ip, function(){
