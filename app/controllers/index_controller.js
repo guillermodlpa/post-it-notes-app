68218e06e52330fd060725d574f8c26cb24dc12d
Guillermo <guillermo@splashthat.com>
2015-09-15 23:03:36 -0400

    project changed from mapins to todo list

--- app/controllers/index_controller.js
+++ app/controllers/index_controller.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 
 // Database name is set on config.yaml, the puphpet file
-var mongoDbUrl = "mongodb://localhost:27017/local_mapins";
+var mongoDbUrl = "mongodb://localhost:27017/local_todo_list";
 
 exports.index = function( request, response, next ) {
 	// response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -40,31 +40,6 @@ exports.increaseCounter = function( request, response, next ) {
 			if (err) return response.end(err);
 			response.end(kittens.length);
 		});
-
-		// var collection = db.collection('clicks');
-
-		// var newClick = { date: new Date(), fromUser: 1 };
-
-		// collection.insert( newClick, function(err, result) {
-
-		// 	if ( err ) {
-		// 		response.end('failure in inset');
-		// 		return;
-		// 	}
-
-		// 	collection.find({fromUser: 1}).toArray(function (err, result) {
-
-		// 		if (err) {
-		// 			response.end(err);
-		// 		} else if (result.length) {
-		// 			response.end('Found:' + result.length);
-		// 		} else {
-		// 			response.end('No document(s) found with defined "find" criteria!');
-		// 		}
-		// 		//Close connection
-		// 		db.close();
-		// 	});
-		// });
 
 	});
 }
