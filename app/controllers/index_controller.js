491f4f118f856cfba6d17cf583c1ca9216ccef3a
Guillermo <guillermo@splashthat.com>
2015-09-15 22:38:09 -0400

    using mongoose for interaction with models

--- app/controllers/index_controller.js
+++ app/controllers/index_controller.js
@@ -2,6 +2,8 @@
 // var mapController = require("./map_controller");
 
 var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+
 // Database name is set on config.yaml, the puphpet file
 var mongoDbUrl = "mongodb://localhost:27017/local_mapins";
 
@@ -13,40 +15,57 @@ exports.index = function( request, response, next ) {
 
 exports.increaseCounter = function( request, response, next ) {
 
-	// Connect to the db
-	MongoClient.connect( mongoDbUrl , function(err, db) {
-		if(err) {
-			response.end('failure in response');
-		}
-		else {
-			// response.out('success in response ');
+	mongoose.connect(mongoDbUrl);
 
-			var collection = db.collection('clicks');
+	var db = mongoose.connection;
+	db.on('error', function(){
+		response.end('failure in response');
+	});
+	db.once('open', function (callback) {
 
-			var newClick = { date: new Date(), fromUser: 1 };
+		var kittenSchema = mongoose.Schema({
+			name: String,
+			// date: Date, default: Date.now
+		});
 
-			collection.insert( newClick, function(err, result) {
+		var Kitten = mongoose.model('Kitten', kittenSchema);
 
-				if ( err ) {
-					response.end('failure in inset');
-					return;
-				}
+		var noisy = new Kitten({ name: 'Noisy' });
+		// response.end(noisy.name); // 'Noisy'
 
-				collection.find({fromUser: 1}).toArray(function (err, result) {
+		// save to db
+		noisy.save(function (err, fluffy) {} );
 
-					if (err) {
-						response.end(err);
-					} else if (result.length) {
-						response.end('Found:' + result.length);
-					} else {
-						response.end('No document(s) found with defined "find" criteria!');
-					}
-					//Close connection
-					db.close();
-				});
-			});
+		Kitten.find(function (err, kittens) {
+			if (err) return response.end(err);
+			response.end(kittens.length);
+		});
 
-		}
+		// var collection = db.collection('clicks');
+
+		// var newClick = { date: new Date(), fromUser: 1 };
+
+		// collection.insert( newClick, function(err, result) {
+
+		// 	if ( err ) {
+		// 		response.end('failure in inset');
+		// 		return;
+		// 	}
+
+		// 	collection.find({fromUser: 1}).toArray(function (err, result) {
+
+		// 		if (err) {
+		// 			response.end(err);
+		// 		} else if (result.length) {
+		// 			response.end('Found:' + result.length);
+		// 		} else {
+		// 			response.end('No document(s) found with defined "find" criteria!');
+		// 		}
+		// 		//Close connection
+		// 		db.close();
+		// 	});
+		// });
+
 	});
 }
 
