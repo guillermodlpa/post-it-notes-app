73bebb0bfbcc4c8689b53c8a01fdc33df94cc156
Guillermo <guillermo@splashthat.com>
2015-09-15 23:14:09 -0400

    added underscore and backbone

--- _tasks/options/copy.js
+++ _tasks/options/copy.js
@@ -2,7 +2,9 @@ module.exports = {
 	components: {
 		expand: true,
 		src: [
-			"node_modules/jquery/dist/jquery.min.js"
+			"node_modules/jquery/dist/jquery.min.js",
+			"node_modules/backbone/backbone-min.js",
+			"node_modules/underscore/underscore-min.js"
 		],
 		dest: "app/public/js/libs",
 		// flatten so the input directory structure doesn't get copied
