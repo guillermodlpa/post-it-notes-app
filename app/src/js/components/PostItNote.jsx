da9d7464fe73f5c6fd3d72240528657734e0f0a1
Guillermo de la Puente <g.puente.allott@gmail.com>
2016-12-17 22:06:11 +0100

    Using correct template for post it note and added propType checking

--- app/src/js/components/PostItNote.jsx
+++ app/src/js/components/PostItNote.jsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
 
 export default class PostItNote extends Component {
   render() {
     return (
       <li className="post-it-note">
-        Post It Note
+        <div className="post-it-note-content">
+          {this.props.content || ''}
+        </div>
+        <div className="post-it-note-button post-it-note-delete">
+          Delete
+        </div>
       </li>
     );
   }
 }
+
+PostItNote.propTypes = {
+  content: PropTypes.string,
+};
